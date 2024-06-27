34d6a36b3654a8730544b724ad2990ded25b47ed
anushreesulibhavi <anushreems2612@gmail.com>
2024-06-27 09:06:41 +0530

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
 // vite.config.js
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+// import { defineConfig } from 'vite';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
 
-export default defineConfig({
-  plugins: [reactRefresh()],
-});
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+// });
