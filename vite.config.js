fc0c7bb16f48b48170628700b3c22149c853c791
anushreesulibhavi <anushreems2612@gmail.com>
2024-07-01 13:09:40 +0530

    Commit message for unstaged changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-// vite.config.js
-// import { defineConfig } from 'vite';
-// import reactRefresh from '@vitejs/plugin-react-refresh';
-
+// // https://vitejs.dev/config/
 // export default defineConfig({
-//   plugins: [reactRefresh()],
-// });
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()]
+});
